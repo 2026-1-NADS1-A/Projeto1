d190980ab46d818493f92043d88f67f57f52a9f2
leonardoencinas <leonardo.encinas@edu.fecap.br>
2026-05-14 23:00:01 -0300

    adiciona o csproj

--- PrototipoMessier/PrototipoMessier/obj/Debug/net8.0-windows/PrototipoMessier.AssemblyInfo.cs
+++ PrototipoMessier/PrototipoMessier/obj/Debug/net8.0-windows/PrototipoMessier.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrototipoMessier")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe6641343da09fc2daffdcc99524f3922618bb2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrototipoMessier")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrototipoMessier")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
